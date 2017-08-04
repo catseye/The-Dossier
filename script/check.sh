c104bc173489502b4ef4df6591e4951951f5079b
Chris Pressey <cpressey@catseye.tc>
2017-08-04 17:01:39 +0100

    Add script to do a quick check of meta-data embedded in articles.

---
+++ script/check.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+feedmark --by-property article/*\ of\ Note.md \
+                       article/Recollected\ Games.md \
+                       article/Lost\ Games.md \
+                       article/Some\ Modern\ Retrogames.md \
+                       article/Classic\ Computer\ Games.md article/Classic\ Text\ Adventures.md
