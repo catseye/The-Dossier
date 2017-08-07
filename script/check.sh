b0263f488f88082a5dc62f30c6b0cf928957b881
Chris Pressey <cpressey@catseye.tc>
2017-08-07 17:36:35 +0100

    Experimentally go forward by adding a schema for Text adventures.

--- script/check.sh
+++ script/check.sh
@@ -6,4 +6,4 @@ feedmark --by-property article/*\ of\ Note.md \
                        article/Some\ Modern\ Retrogames.md \
                        article/Classic\ Computer\ Games.md article/Classic\ Text\ Adventures.md
 
-# ./bin/feedmark --output-markdown ../The-Dossier/article/Text\ Adventures\ of\ Note.md --property-priority-order='written by,published by,available for,date released,written using,with graphics,treasure-oriented,personally finished,wikipedia,entry,walkthrough,play online'
+feedmark --check-against schema/Text\ adventure.md ../The-Dossier/article/Text\ Adventures\ of\ Note.md ../The-Dossier/article/Classic\ Text\ Adventures.md
