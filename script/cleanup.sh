cf488bf36dc5a443b8ed96b6a3ad19a7c1a2bb40
Chris Pressey <cpressey@catseye.tc>
2017-08-15 13:47:28 +0100

    Create a refdex and input on subsequent passes - will happen soon.

--- script/cleanup.sh
+++ script/cleanup.sh
@@ -2,29 +2,36 @@
 
 ARTICLES=../The-Dossier/article
 
-feedmark --by-property article/*\ of\ Note.md \
-                       article/Recollected\ Games.md \
-                       article/Lost\ Games.md \
-                       article/Some\ Modern\ Retrogames.md \
-                       article/Classic\ Computer\ Games.md article/Classic\ Text\ Adventures.md >/dev/null
+(cd $ARTICLES && feedmark --output-refdex *\ of\ Note.md \
+                          Recollected\ Games.md \
+                          Some\ Modern\ Retrogames.md \
+                          Classic\ Computer\ Games.md \
+                          Classic\ Text\ Adventures.md \
+                 >../refdex.json)
 
 # non-lists
-feedmark "$ARTICLES/A Basic Theory of Video Games.md" \
+feedmark --input-refdex=refdex.json \
+         "$ARTICLES/A Basic Theory of Video Games.md" \
          "$ARTICLES/Blurry Memories of DOS Programming.md" \
          "$ARTICLES/Perspective on Text Adventures.md" \
          "$ARTICLES/The Aesthetics of Esolangs.md" \
          "$ARTICLES/Retrospective on Language Design.md" \
          --rewrite-markdown || exit 1
 
-feedmark --check-against-schema=schema/Text\ adventure.md \
-         ../The-Dossier/article/Text\ Adventures\ of\ Note.md ../The-Dossier/article/Classic\ Text\ Adventures.md \
+#lists
+feedmark --input-refdex=refdex.json \
+         --check-against-schema=schema/Text\ adventure.md \
+         $ARTICLES/Text\ Adventures\ of\ Note.md \
+         $ARTICLES/Classic\ Text\ Adventures.md \
          --rewrite-markdown || exit 1
 
-feedmark --check-against-schema=schema/Book.md \
-         ../The-Dossier/article/An\ Esolang\ Reading\ List.md \
+feedmark --input-refdex=refdex.json \
+         --check-against-schema=schema/Book.md \
+         "$ARTICLES/An Esolang Reading List.md" \
          --rewrite-markdown || exit 1
 
-feedmark --check-against-schema=schema/Video\ game.md \
+feedmark --input-refdex=refdex.json \
+         --check-against-schema=schema/Video\ game.md \
          "$ARTICLES/Some Modern Retrogames.md" \
          "$ARTICLES/Apple II Games of Note.md" \
          --rewrite-markdown || exit 1
