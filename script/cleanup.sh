4a6a5accad7fe1e75f2ebfecc8d136d667fe5ddd
Chris Pressey <cpressey@catseye.tc>
2022-09-19 14:29:14 +0100

    Produce two disjoint refdexes during cleanup.

--- script/cleanup.sh
+++ script/cleanup.sh
@@ -3,8 +3,6 @@
 ARTICLES=../The-Dossier/article
 
 (cd $ARTICLES && feedmark --output-refdex --output-refdex-single-filename \
-                          "20th-Century Visual Artists of Note.md" \
-                          "A Note on Items of Note.md" \
                           "Apple II Games of Note.md" \
                           "Atari 2600 Games of Note.md" \
                           "Commodore 64 Games of Note.md" \
@@ -18,11 +16,17 @@ ARTICLES=../The-Dossier/article
                           "Some Modern Retrogames.md" \
                           "Classic Computer Games.md" \
                           "Classic Text Adventures.md" \
+                 >../dossier-games-refdex.json) || exit 1
+
+(cd $ARTICLES && feedmark --output-refdex --output-refdex-single-filename \
+                          "20th-Century Visual Artists of Note.md" \
                           "An Esolang Reading List.md" \
-                 >../refdex.json) || exit 1
+                 >../dossier-refdex.json) || exit 1
+
+REFDEXES=dossier-refdex.json,dossier-games-refdex.json
 
 # non-lists
-feedmark --input-refdex=refdex.json \
+feedmark --input-refdexes=$REFDEXES \
          "$ARTICLES/A Basic Theory of Video Games.md" \
          "$ARTICLES/Blurry Memories of DOS Programming.md" \
          "$ARTICLES/Perspective on Text Adventures.md" \
@@ -32,18 +36,18 @@ feedmark --input-refdex=refdex.json \
          --rewrite-markdown || exit 1
 
 #lists
-feedmark --input-refdex=refdex.json \
+feedmark --input-refdexes=$REFDEXES \
          --check-against-schema=schema/Text\ adventure.md \
          $ARTICLES/Text\ Adventures\ of\ Note.md \
          $ARTICLES/Classic\ Text\ Adventures.md \
          --rewrite-markdown || exit 1
 
-feedmark --input-refdex=refdex.json \
+feedmark --input-refdexes=$REFDEXES \
          --check-against-schema=schema/Book.md \
          "$ARTICLES/An Esolang Reading List.md" \
          --rewrite-markdown || exit 1
 
-feedmark --input-refdex=refdex.json \
+feedmark --input-refdexes=$REFDEXES \
          --check-against-schema=schema/Video\ game.md \
          "$ARTICLES/Some Modern Retrogames.md" \
          "$ARTICLES/Commodore 64 Games of Note.md" \
@@ -52,13 +56,13 @@ feedmark --input-refdex=refdex.json \
          "$ARTICLES/Computer Sports Games of Note.md" \
          --rewrite-markdown || exit 1
 
-feedmark --input-refdex=refdex.json \
+feedmark --input-refdexes=$REFDEXES \
          --check-against-schema=schema/Computer\ game.md \
          "$ARTICLES/Classic Computer Games.md" \
          "$ARTICLES/Computer Games of Note.md" \
          --rewrite-markdown || exit 1
 
-feedmark --input-refdex=refdex.json \
+feedmark --input-refdexes=$REFDEXES \
          --check-against-schema=schema/Lost\ game.md \
          "$ARTICLES/Lost Games.md" \
          --rewrite-markdown || exit 1
