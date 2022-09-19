323d8451ae9b23804c54137163c4dba9dca7fa06
Chris Pressey <cpressey@catseye.tc>
2022-09-19 14:09:51 +0100

    Make this list of files explicit.

--- script/cleanup.sh
+++ script/cleanup.sh
@@ -3,7 +3,17 @@
 ARTICLES=../The-Dossier/article
 
 (cd $ARTICLES && feedmark --output-refdex --output-refdex-single-filename \
-                          *\ of\ Note.md \
+                          "20th-Century Visual Artists of Note.md" \
+                          "A Note on Items of Note.md" \
+                          "Apple II Games of Note.md" \
+                          "Atari 2600 Games of Note.md" \
+                          "Commodore 64 Games of Note.md" \
+                          "Computer Games of Note.md" \
+                          "Computer Sports Games of Note.md" \
+                          "Licensed-Character Video Games of Note.md" \
+                          "Role-Playing Games of Note.md" \
+                          "Text Adventures of Note.md" \
+                          "Video Games of Note.md" \
                           "Recollected Games.md" \
                           "Some Modern Retrogames.md" \
                           "Classic Computer Games.md" \
