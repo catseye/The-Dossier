4a2948a11987ab87c98446f17ed976af8fa45fea
Chris Pressey <cpressey@catseye.tc>
2022-09-29 06:26:12 +0100

    Games of Note have moved out, jettison the infrastructure.

--- script/cleanup.sh
+++ script/cleanup.sh
@@ -7,7 +7,7 @@ ARTICLES=../The-Dossier/article
                           "An Esolang Reading List.md" \
                  >../dossier-refdex.json) || exit 1
 
-REFDEXES=dossier-refdex.json,../Chrysoberyl/misc-refdex/sgon-refdex.json
+REFDEXES=dossier-refdex.json
 
 # non-lists
 feedmark --input-refdexes=$REFDEXES \
@@ -21,32 +21,6 @@ feedmark --input-refdexes=$REFDEXES \
 
 #lists
 feedmark --input-refdexes=$REFDEXES \
-         --check-against-schema=schema/Text\ adventure.md \
-         $ARTICLES/Text\ Adventures\ of\ Note.md \
-         $ARTICLES/Classic\ Text\ Adventures.md \
-         --rewrite-markdown || exit 1
-
-feedmark --input-refdexes=$REFDEXES \
          --check-against-schema=schema/Book.md \
          "$ARTICLES/An Esolang Reading List.md" \
-         --rewrite-markdown || exit 1
-
-feedmark --input-refdexes=$REFDEXES \
-         --check-against-schema=schema/Video\ game.md \
-         "$ARTICLES/Some Modern Retrogames.md" \
-         "$ARTICLES/Commodore 64 Games of Note.md" \
-         "$ARTICLES/Apple II Games of Note.md" \
-         "$ARTICLES/Atari 2600 Games of Note.md" \
-         "$ARTICLES/Computer Sports Games of Note.md" \
-         --rewrite-markdown || exit 1
-
-feedmark --input-refdexes=$REFDEXES \
-         --check-against-schema=schema/Computer\ game.md \
-         "$ARTICLES/Classic Computer Games.md" \
-         "$ARTICLES/Computer Games of Note.md" \
-         --rewrite-markdown || exit 1
-
-feedmark --input-refdexes=$REFDEXES \
-         --check-against-schema=schema/Lost\ game.md \
-         "$ARTICLES/Lost Games.md" \
          --rewrite-markdown || exit 1
